d4504e3f7a92da1b94524b79c996048ed6562000
Jae Sung Jang <jaesungj@umich.edu>
2016-03-08 09:52:58 -0800

    uploaded sql

---
+++ inventory/sql/tbl_create.sql
@@ -0,0 +1,10 @@
+create database inventory
+
+use inventory
+
+create table product(barcode VARCHAR(255), productname VARCHAR(255), currentprice DOUBLE);
+
+insert into product values("xx", "xx", 1)
+insert into product values("xx1", "xx1", 2)
+insert into product values("xx2", "xx2", 3)
+insert into product values("xx3", "xx3", 4)
